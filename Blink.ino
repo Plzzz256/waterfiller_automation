a17b9b595ef2e91a71b7330dc1aec3b6fae7a091
Topzaxa1112 <60424151+Topzaxa1112@users.noreply.github.com>
2020-03-04 16:16:24 +0700

    relay_pump

--- Blink.ino
+++ Blink.ino
@@ -1,9 +1,16 @@
 int ledPin = 2;
-
+int ledPin1 = 3;
 
 void setup()
 {
   pinMode(ledPin, OUTPUT);
-  pinMode(ledPin1, OUTPUT);
 } 
 
+void loop()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(2000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+  digitalWrite(ledPin1, HIGH);
+}
