00dc23a95f4707a34e34bc13bbf128fcd34ce308
Topzaxa1112 <60424151+Topzaxa1112@users.noreply.github.com>
2020-03-11 15:38:52 +0700

    all_code_11_03

---
+++ main_code_update_2/0001/0001.ino
@@ -0,0 +1,27 @@
+#include <AccelStepper.h>
+#define dirPin 2 // Board Control Motor pin
+#define motorInterfaceType 1
+#define stepPin 3 // Board Control Motor pin
+#define stepsPerRevolution 33
+AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
+void setup() 
+{
+  // put your setup code here, to run once:
+ pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  stepper.setMaxSpeed(1000);
+}
+
+void loop() 
+{
+  // Set the current position to 0:
+  stepper.setCurrentPosition(0);
+  // Run the motor forward at 200 steps/second until the motor reaches 400 steps (2 revolutions):
+  while(stepper.currentPosition() != 400)
+  {
+    stepper.setSpeed(200);
+    stepper.runSpeed();
+  }
+  delay(1000);
+}
+  
